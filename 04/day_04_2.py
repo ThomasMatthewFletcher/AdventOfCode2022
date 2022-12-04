cc274549c55ae880dd7303220734571442a5c711
Tom Fletcher <thomasmatthewfletcher@gmail.com>
2022-12-04 14:11:50 +0000

    Complete day 04

---
+++ 04/day_04_2.py
@@ -0,0 +1,37 @@
+from dataclasses import dataclass
+from typing import Self
+
+@dataclass
+class Range:
+    min: int
+    max: int
+
+    def contains(self, other: Self):
+        return self.min <= other.min and self.max >= other.max
+
+    def overlaps(self, other: Self):
+        return not (self.min > other.max or self.max < other.min)
+
+Pair = tuple[Range, Range]
+
+def main():
+    pairs = read_pairs()
+
+    count = sum(1 for pair in pairs if pair[0].overlaps(pair[1]))
+    print('Count:', count)
+
+
+def read_pairs():
+    with open('04/input.txt', encoding='ascii') as file:
+        return [parse_pair(line.strip()) for line in file]
+
+def parse_pair(line: str) -> Pair:
+    parts = line.split(',')
+    return (parse_range(parts[0]), parse_range(parts[1]))
+
+def parse_range(range_str: str):
+    parts = range_str.split('-')
+    return Range(int(parts[0]), int(parts[1]))
+
+if __name__ == '__main__':
+    main()
